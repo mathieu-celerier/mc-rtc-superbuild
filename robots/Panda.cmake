2b202a884efb36218c4334e652016e21ccc8c512
Mathieu Celerier <mathieu.celerier@aist.go.jp>
2025-11-12 10:29:31 +0900

    [fix] Fix some issues with Panda's cmake for ROS2 and AddProject order

--- robots/Panda.cmake
+++ robots/Panda.cmake
@@ -28,18 +28,30 @@ if(Panda_DEPENDENCIES_FROM_SOURCE)
   AddProject(
     libfranka
     GITHUB frankaemika/libfranka
-    GIT_TAG origin/0.8.0
+    GIT_TAG origin/release-0.17.0
+    SKIP_TEST
   )
   set(mc_panda_DEPENDS libfranka)
   if(WITH_ROS_SUPPORT)
-    AddCatkinProject(
-      franka_ros
-      GITHUB frankaemika/franka_ros
-      GIT_TAG origin/0.8.1
-      WORKSPACE mc_rtc_ws
-      DEPENDS libfranka
-    )
-    list(APPEND mc_panda_DEPENDS franka_ros)
+    if(ROS_IS_ROS2)
+      AddCatkinProject(
+        franka_description
+        GITHUB frankarobotics/franka_description
+        GIT_TAG origin/jazzy
+        WORKSPACE mc_rtc_ws
+        DEPENDS libfranka
+      )
+      list(APPEND mc_panda_DEPENDS franka_description)
+    else()
+      AddCatkinProject(
+        franka_ros
+        GITHUB frankaemika/franka_ros
+        GIT_TAG origin/0.8.1
+        WORKSPACE mc_rtc_ws
+        DEPENDS libfranka
+      )
+      list(APPEND mc_panda_DEPENDS franka_ros)
+    endif()
   endif()
 else()
   if(NOT DPKG OR NOT WITH_ROS_SUPPORT)
@@ -58,15 +70,6 @@ AddProject(
   DEPENDS mc_rtc ${mc_panda_DEPENDS}
 )
 
-if(WITH_PandaLIRMM)
-  AddProject(
-    mc_panda_lirmm
-    GITHUB jrl-umi3218/mc_panda_lirmm
-    GIT_TAG origin/main
-    DEPENDS mc_panda
-  )
-endif()
-
 if(WITH_MC_FRANKA)
   AptInstall(libcap2-bin) # for setcap
   AddProject(
@@ -74,5 +77,14 @@ if(WITH_MC_FRANKA)
     GITHUB jrl-umi3218/mc_franka
     GIT_TAG origin/master
     DEPENDS mc_rtc mc_panda
+  )
+endif()
+
+if(WITH_PandaLIRMM)
+  AddProject(
+    mc_panda_lirmm
+    GITHUB jrl-umi3218/mc_panda_lirmm
+    GIT_TAG origin/main
+    DEPENDS mc_panda
   )
 endif()

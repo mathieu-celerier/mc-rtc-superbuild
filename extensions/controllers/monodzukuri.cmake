289c9622e201aa201ccc2eb4ee4d0e1ee91f8c8e
Mathieu Celerier <mathieu.celerier@aist.go.jp>
2025-11-12 10:36:53 +0900

    [extensions] Comment the .gitignore in extensions folder to save all extensions and local.cmake

---
+++ extensions/controllers/monodzukuri.cmake
@@ -0,0 +1,5 @@
+AddProject(monodzukuri_controller
+	GITHUB_PRIVATE bastien-muraccioli/monodzukuri2024_kinova_demo
+  GIT_TAG origin/rss2025
+	DEPENDS mc_rtc
+) 
---
+++ extensions/controllers/torque_test.cmake
@@ -0,0 +1,5 @@
+AddProject(mc_torque_example
+  GITHUB ThomasDuvinage/mc_torque_example
+  GIT_TAG origin/main
+  DEPENDS mc_rtc
+)

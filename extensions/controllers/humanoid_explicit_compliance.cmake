a3bc6663c226e59a47472d4a7af8194f1003b808
Mathieu Celerier <mathieu.celerier@aist.go.jp>
2025-11-12 10:36:53 +0900

    [extensions] Comment the .gitignore in extensions folder to save all extensions and local.cmake

---
+++ extensions/controllers/humanoid_explicit_compliance.cmake
@@ -0,0 +1,5 @@
+AddProject(humanoid-explicitly-compliant
+  GITHUB_PRIVATE mathieu-celerier/humanoid-explicitly-compliant
+  GIT_TAG origin/main
+  DEPENDS mc_rtc
+)
---
+++ extensions/plugins/mc_residual_estimation.cmake
@@ -0,0 +1,5 @@
+AddProject(mc_residual_estimation
+	GITHUB_PRIVATE bastien-muraccioli/mc_residual_estimation
+  GIT_TAG origin/topic/HumanoidResidual
+	DEPENDS mc_rtc
+)

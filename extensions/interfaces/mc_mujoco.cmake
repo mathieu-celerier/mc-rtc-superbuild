70959926f921ceba23dbb0cca787fd8119efb009
Mathieu Celerier <mathieu.celerier@aist.go.jp>
2025-11-21 15:20:30 +0900

    [extensions] Update mc_mujoco's branch

--- extensions/interfaces/mc_mujoco.cmake
+++ extensions/interfaces/mc_mujoco.cmake
@@ -45,7 +45,7 @@ endif()
 if(WITH_Kinova)
   AddProject(kinova_mj_description
     GITHUB_PRIVATE mathieu-celerier/kinova_mj_description
-    GIT_TAG origin/main
+    GIT_TAG origin/main-external-forces
     DEPENDS mc_mujoco mc_kinova
   )
 endif()

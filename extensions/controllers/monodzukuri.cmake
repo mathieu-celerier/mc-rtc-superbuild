e62ea9c22273ef54765679c924d87c0067796f37
Mathieu Celerier <mathieu.celerier@aist.go.jp>
2026-03-05 16:25:24 +0900

    Branches updates

--- extensions/controllers/monodzukuri.cmake
+++ extensions/controllers/monodzukuri.cmake
@@ -1,5 +1,7 @@
+AptInstall(libglu1-mesa-dev freeglut3-dev libglfw3-dev)
+
 AddProject(monodzukuri_controller
 	GITHUB_PRIVATE bastien-muraccioli/monodzukuri2024_kinova_demo
-  GIT_TAG origin/rss2025
+  GIT_TAG origin/rss2025_new_implementation
 	DEPENDS mc_rtc
 ) 

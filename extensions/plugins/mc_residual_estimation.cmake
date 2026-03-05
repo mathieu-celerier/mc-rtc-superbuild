c9877803ffbe686f5b51cecb8d35d7a2a3d58164
Mathieu Celerier <mathieu.celerier@aist.go.jp>
2026-03-05 16:25:24 +0900

    Branches updates

--- extensions/plugins/mc_residual_estimation.cmake
+++ extensions/plugins/mc_residual_estimation.cmake
@@ -1,5 +1,5 @@
 AddProject(mc_residual_estimation
-	GITHUB_PRIVATE bastien-muraccioli/mc_residual_estimation
-  GIT_TAG origin/topic/HumanoidResidual
+	GITHUB_PRIVATE mathieu-celerier/mc_residual_estimation
+  GIT_TAG origin/topic/HumanoidResidual_new_implementation
 	DEPENDS mc_rtc
 )

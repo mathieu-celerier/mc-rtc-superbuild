9543d6b07d60adddcd91d2fcb94db87278ed0c2c
Mathieu Celerier <mathieu.celerier@aist.go.jp>
2026-03-09 16:42:58 +0900

    Add mc_ros_force_sensor plugin

--- extensions/controllers/monodzukuri.cmake
+++ extensions/controllers/monodzukuri.cmake
@@ -3,5 +3,5 @@ AptInstall(libglu1-mesa-dev freeglut3-dev libglfw3-dev)
 AddProject(monodzukuri_controller
 	GITHUB_PRIVATE bastien-muraccioli/monodzukuri2024_kinova_demo
   GIT_TAG origin/rss2025_new_implementation
-	DEPENDS mc_rtc
+	DEPENDS mc_rtc mc_ros_force_sensor
 ) 

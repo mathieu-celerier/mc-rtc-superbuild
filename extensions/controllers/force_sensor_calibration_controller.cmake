e1a2d17852fa3449f45e6967994e2f6be2329e2f
Mathieu Celerier <mathieu.celerier@aist.go.jp>
2026-03-24 08:59:59 +0900

    Reflect changes required for test in waterfron on 03/18

---
+++ extensions/controllers/force_sensor_calibration_controller.cmake
@@ -0,0 +1,13 @@
+AptInstall(libgoogle-glog-dev)
+AddProject(ceres-solver
+  GITHUB ceres-solver/ceres-solver
+  GIT_TAG 2.1.0
+  CMAKE_ARGS -DBUILD_BENCHMARKS:BOOL=OFF -DBUILD_EXAMPLES:BOOL=OFF
+  SKIP_TEST
+)
+AddProject(
+  mc_force_sensor_calibration_controller
+  GITHUB mathieu-celerier/mc_force_sensor_calibration_controller
+  GIT_TAG origin/topic/kinova-calib
+  DEPENDS mc_rtc
+)

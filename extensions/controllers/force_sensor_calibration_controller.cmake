8587f9dae4bf48e594edd46e8c63b98bd9210e88
Mathieu Celerier <mathieu.celerier@aist.go.jp>
2026-04-01 21:24:52 +0900

    Update force_sensor_calibration_controller.cmake for proper setup

--- extensions/controllers/force_sensor_calibration_controller.cmake
+++ extensions/controllers/force_sensor_calibration_controller.cmake
@@ -2,12 +2,14 @@ AptInstall(libgoogle-glog-dev)
 AddProject(ceres-solver
   GITHUB ceres-solver/ceres-solver
   GIT_TAG 2.1.0
-  CMAKE_ARGS -DBUILD_BENCHMARKS:BOOL=OFF -DBUILD_EXAMPLES:BOOL=OFF
+  CMAKE_ARGS -DBUILD_BENCHMARKS:BOOL=OFF
+             -DBUILD_EXAMPLES:BOOL=OFF
+             -DBUILD_SHARED_LIBS:BOOL=ON
   SKIP_TEST
 )
 AddProject(
   mc_force_sensor_calibration_controller
   GITHUB mathieu-celerier/mc_force_sensor_calibration_controller
   GIT_TAG origin/topic/kinova-calib
-  DEPENDS mc_rtc
+  DEPENDS mc_rtc ceres-solver
 )
